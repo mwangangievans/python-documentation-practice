db8bcaf2833d404532bd755e79230fadd9a23197
Evans Mwangangi <emwangangi@webtribe.co.ke>
2023-03-07 11:25:05 +0300

    migration

--- Project_1/tutorial/snippets/models.py
+++ Project_1/tutorial/snippets/models.py
@@ -6,7 +6,8 @@ from pygments.styles import get_all_styles
 
 LEXERS = [item for item in  get_all_lexers() if item[1]]
 LANGUAGEE_CHOICES = sorted([(item[1][0],item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item , item) for item in get_all_lexers()])
+# STYLE_CHOICES = sorted([(item , item) for item in get_all_lexers()])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
--- Project_1/tutorial/tutorial/settings.py
+++ Project_1/tutorial/tutorial/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': 
+    {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'First_project',
+       'USER': 'postgres',
+       'PASSWORD': '34552344',
+       'HOST': 'localhost',
+       'PORT': '5433',
+   }
 }
 
 
