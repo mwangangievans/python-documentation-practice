f61587203fda3bf45b97fe330af57853d2116859
Evans Mwangangi <emwangangi@webtribe.co.ke>
2023-03-08 14:36:16 +0300

    function based views

--- Project_1/tutorial/snippets/views.py
+++ Project_1/tutorial/snippets/views.py
@@ -6,7 +6,7 @@ from snippets.serializers import SnippetSerializer
 
 
 @api_view(['GET','POST'])
-def snippet_list(request):
+def snippet_list(request , format=None):
     """
     list all code snippets , or create a new snippet
 
@@ -26,7 +26,7 @@ def snippet_list(request):
     
 
 @api_view(['GET','PUT','DELETE'])
-def snippet_detail(request , pk):
+def snippet_detail(request , pk ,format=None):
     """
     Retrieve , update or delete a code spippets
     
