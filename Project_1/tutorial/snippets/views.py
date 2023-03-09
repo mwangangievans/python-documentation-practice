a0e71c71c1bf82fa9281c4bab6d6368f8b1de528
Evans Mwangangi <emwangangi@webtribe.co.ke>
2023-03-09 14:41:11 +0300

    customised mixins

--- Project_1/tutorial/snippets/views.py
+++ Project_1/tutorial/snippets/views.py
@@ -1,30 +1,13 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import mixins , generics
+from rest_framework import generics
 
 
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class  = SnippetSerializer
 
-class SnippetList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self , request , *arg , **kwargs):
-        return self.list(request , *arg , **kwargs)
-    
-    def post(self,request , *arg , **kwargs):
-        return self.create(request , *arg , **kwargs)
-    
-
-class SnippetDetail(mixins.RetrieveModelMixin , mixins.UpdateModelMixin , mixins.DestroyModelMixin,generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-
-    def get(self , request , *arg ,**kwargs):
-        return self.retrieve(request , *arg ,**kwargs )
-    
-    def update(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self , request , *args , **kwargs):
-        return self.destroy(request , *args , **kwargs)
-  
